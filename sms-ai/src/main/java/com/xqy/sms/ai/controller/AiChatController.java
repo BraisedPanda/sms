debc6fbcd4248d7ad7e843112311cdba5928de84
yuluo <987433289@qq.com>
2026-08-13 21:25:04 +0800

    [2026-08-13]初次接入Open AI

--- sms-ai/src/main/java/com/xqy/sms/ai/controller/AiChatController.java
+++ sms-ai/src/main/java/com/xqy/sms/ai/controller/AiChatController.java
@@ -1,11 +1,16 @@
 package com.xqy.sms.ai.controller;
 
+import com.xqy.sms.ai.service.AiChatAssistant;
 import com.xqy.sms.common.dto.ApiResponse;
 import com.xqy.sms.student.api.entity.StudentDTO;
 import com.xqy.sms.student.api.service.StudentService;
+import dev.langchain4j.model.openai.OpenAiChatModel;
+
+import dev.langchain4j.service.AiServices;
 import org.apache.dubbo.config.annotation.DubboReference;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -17,10 +22,23 @@ public class AiChatController {
     @DubboReference
     private StudentService studentService;
 
+    private final AiChatAssistant aiChatAssistant;
+
+    public AiChatController(OpenAiChatModel openAiChatModel) {
+        this.aiChatAssistant = AiServices.create(AiChatAssistant.class, openAiChatModel);
+    }
+
     @GetMapping("/test1")
     public ApiResponse<?> test1() {
         List<StudentDTO> list =  studentService.listStudents();
         return ApiResponse.success(list);
+    }
+
+
+    @GetMapping("/sample-chat")
+    public ApiResponse<?> sampleChat(@RequestParam String question) {
+        String answer = aiChatAssistant.chat(question);
+        return ApiResponse.success(answer);
     }
 
 
---
+++ sms-ai/src/main/java/com/xqy/sms/ai/service/AiChatAssistant.java
@@ -0,0 +1,7 @@
+package com.xqy.sms.ai.service;
+
+
+public interface AiChatAssistant {
+
+    String chat( String question);
+}
